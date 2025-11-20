eb08d67446f6f164bce4b464df25e6b4930eed05
Sighhhsmh <100480326+Sighhhsmh@users.noreply.github.com>
2025-11-20 21:29:57 +0500

    Update firestore.js

--- backend/firestore.js
+++ backend/firestore.js
@@ -1,7 +1,7 @@
 const admin =  require("firebase-admin");
 require('dotenv').config();
 
-const serviceAccount = JSON.parse(process.env['Firebase-Service-Account']);
+const serviceAccount = JSON.parse(process.env['FIREBASE_SERVICE_ACCOUNT']);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -10,5 +10,6 @@ admin.initializeApp({
 const db = admin.firestore();
 
 module.exports = db;
+
 
 
